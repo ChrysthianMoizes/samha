4708293491629606ac638ad8dad38c6e5cec2390
Chrysthian Moizes <chrysthianmoizes@gmail.com>
2018-03-12 20:30:38 -0300

    Criação de controladora principal do sistema

---
+++ src/cci/CtrlPrincipal.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cci;
+
+/**
+ *
+ * @author chrys
+ */
+public class CtrlPrincipal {
+    
+    public static void main(String args[]){
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }   
+        }catch(Exception e){}
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                //new CIInterface();
+            }   
+        });
+    }
+    
+}
