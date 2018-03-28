f62e1adf1090ed50de8a3cba2594a111838a4a5b
Chrysthian Moizes <chrysthianmoizes@gmail.com>
2018-03-27 23:38:38 -0300

    Primeira tela do sistema

--- src/cci/CtrlPrincipal.java
+++ src/cci/CtrlPrincipal.java
@@ -1,20 +1,24 @@
 package cci;
 
 import cgd.Config;
+import cih.FrmPrincipal;
+import cih.FrmValidarAcesso;
+import java.awt.Frame;
 
-public class CtrlPrincipal {
+public final class CtrlPrincipal {
     
-    private Config config;
+    private final Config config;
+    private Frame frmValidarAcesso;
 
     public CtrlPrincipal() {
         config = new Config();
-    
+        instanciarFrameValidarAcesso();
     }
     
     public static void main(String args[]){
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName())) {
+                if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
@@ -28,4 +32,8 @@ public class CtrlPrincipal {
         });
     }
     
+    public void instanciarFrameValidarAcesso(){
+        frmValidarAcesso = new FrmValidarAcesso(this);
+        frmValidarAcesso.setVisible(true);
+    }  
 }
---
+++ src/cih/FrmValidarAcesso.java
@@ -0,0 +1,133 @@
+package cih;
+
+import cci.CtrlPrincipal;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+
+public class FrmValidarAcesso extends javax.swing.JFrame {
+
+    private CtrlPrincipal ctrlPrincipal;
+
+    public FrmValidarAcesso(CtrlPrincipal ctrlPrincipal) {
+        this.ctrlPrincipal = ctrlPrincipal;
+        initComponents();
+        //ImageIcon icone = ciInterface.setarIconesJanela();
+        //setIconImage(icone.getImage());
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabelLogo = new javax.swing.JLabel();
+        jPanelLogin = new javax.swing.JPanel();
+        jLabelUsuario = new javax.swing.JLabel();
+        jLabelSenha = new javax.swing.JLabel();
+        jTextFieldLogin = new javax.swing.JTextField();
+        jPasswordFieldSenha = new javax.swing.JPasswordField();
+        jButtonEntrar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Validar Acesso");
+        setResizable(false);
+        getContentPane().setLayout(new java.awt.GridLayout(0, 1));
+
+        jLabelLogo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        getContentPane().add(jLabelLogo);
+
+        jPanelLogin.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        jLabelUsuario.setText("Usuário:");
+
+        jLabelSenha.setText("Senha:");
+
+        jPasswordFieldSenha.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jPasswordFieldSenhaKeyPressed(evt);
+            }
+        });
+
+        jButtonEntrar.setText("Entrar");
+        jButtonEntrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonEntrarActionPerformed(evt);
+            }
+        });
+        jButtonEntrar.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jButtonEntrarKeyPressed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanelLoginLayout = new javax.swing.GroupLayout(jPanelLogin);
+        jPanelLogin.setLayout(jPanelLoginLayout);
+        jPanelLoginLayout.setHorizontalGroup(
+            jPanelLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelLoginLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanelLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTextFieldLogin)
+                    .addGroup(jPanelLoginLayout.createSequentialGroup()
+                        .addGroup(jPanelLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabelUsuario)
+                            .addComponent(jLabelSenha))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(jPasswordFieldSenha)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelLoginLayout.createSequentialGroup()
+                        .addGap(0, 252, Short.MAX_VALUE)
+                        .addComponent(jButtonEntrar)))
+                .addContainerGap())
+        );
+        jPanelLoginLayout.setVerticalGroup(
+            jPanelLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelLoginLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabelUsuario)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextFieldLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabelSenha)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPasswordFieldSenha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButtonEntrar)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        getContentPane().add(jPanelLogin);
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButtonEntrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonEntrarActionPerformed
+        
+        String login = jTextFieldLogin.getText();
+        String senha = jPasswordFieldSenha.getText();
+        
+        //ciInterface.getCiGeral().validarAcesso(login, senha);
+    }//GEN-LAST:event_jButtonEntrarActionPerformed
+
+    private void jPasswordFieldSenhaKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jPasswordFieldSenhaKeyPressed
+        if(evt.getKeyCode() == KeyEvent.VK_ENTER){
+            jButtonEntrarActionPerformed(null);
+        }
+    }//GEN-LAST:event_jPasswordFieldSenhaKeyPressed
+
+    private void jButtonEntrarKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jButtonEntrarKeyPressed
+        if(evt.getKeyCode() == KeyEvent.VK_ENTER){
+            jButtonEntrarActionPerformed(null);
+        }
+    }//GEN-LAST:event_jButtonEntrarKeyPressed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButtonEntrar;
+    private javax.swing.JLabel jLabelLogo;
+    private javax.swing.JLabel jLabelSenha;
+    private javax.swing.JLabel jLabelUsuario;
+    private javax.swing.JPanel jPanelLogin;
+    private javax.swing.JPasswordField jPasswordFieldSenha;
+    private javax.swing.JTextField jTextFieldLogin;
+    // End of variables declaration//GEN-END:variables
+}
